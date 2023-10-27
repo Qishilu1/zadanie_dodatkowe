15ee0962eff255c7809237e7e0ee3e5d9ae70c3a
Jakub Pudłowski <pudlowskikuba@gmail.com>
2023-10-27 16:54:57 +0200

    skonczenie rejestracji i logowania
    skonczona strona rejestracji i logowania. mozliwe pozniejsze ich poprawki.

--- register.php
+++ register.php
@@ -4,6 +4,7 @@
 		<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="style.css">
 		<title>System ERP - zadanie dodatkowe</title>
+
 	</head>
 	
 	<header>
@@ -12,9 +13,28 @@
 	
 	<body>
 
-		<main id="przyciski">
-			<a href="dodawanie.php">Dodawanie</a>
-			<a href="przegladanie.php">Przeglądanie</a>
+		<main id="rejestracja">
+			<p><b>Rejestracja</b></p>
+			<div id="register">
+				<form action="register.php" method="POST" id="rejestracja">
+					<label for="login"><b>Login:</b></label><br><br>
+					<input type="text" id="login" name="login" placeholder="Login"><br><br><br><br>
+					
+					<label for="passwd"><b>Hasło:</b></label><br><br>
+					<input type="password" id="passwd" name="passwd" placeholder="Hasło"><br>
+					
+					<input type="checkbox" id="pokaz_haslo">Pokaż hasło
+				</form>
+
+				<script type="text/javascript" src="skrypt_haslo.js"></script>
+
+				
+				<div id="przyciski_login">
+					<a href="login.php">Logowanie</a>
+					<button type="submit" form="rejestracja" value="submit">Utwórz konto</button>
+				</div>
+			</div>
+
 		</main>
 	</body>
 	
